b7c5886d06f0e7733ede62f5f3d4358a4b8b14c9
Emon Hossain <emondd4@gmail.com>
2020-12-06 01:01:47 +0600

    6/12/2020 01.01 AM

--- app/src/main/java/com/emon/emonsassessment/Activities/MainActivity.java
+++ app/src/main/java/com/emon/emonsassessment/Activities/MainActivity.java
@@ -3,26 +3,44 @@ package com.emon.emonsassessment.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.emon.emonsassessment.R;
 import com.emon.emonsassessment.RoomModel.User;
-import com.google.gson.Gson;
 
-import java.io.Serializable;
-import java.util.ArrayList;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText SearchEditText;
     private TextView username;
-    private Button Search,Profile;
+    private Button Search, Profile;
     private RecyclerView recyclerView;
     private User my_info;
 
@@ -50,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         Profile = findViewById(R.id.ProfileButton);
         recyclerView = findViewById(R.id.RecyclerView);
 
-        username.setText(sharedPreferences.getString("KEY_USERNAME",null));
+        username.setText(sharedPreferences.getString("KEY_USERNAME", null));
 
         OnClick();
     }
@@ -61,17 +79,54 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                String extra = SearchEditText.getText().toString();
+                String url = "https://api.tvmaze.com/search/shows?q=" + extra;
+
+
+                getJSON(url);
+
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//
+//                                Log.e( "onResponse:", response);
+//
+//                                try {
+//                                    JSONObject jsonObject = new JSONObject(response);
+//
+//                                } catch (JSONException e) {
+//                                    e.printStackTrace();
+//                                }
+//                            }
+//                        },
+//                        new Response.ErrorListener() {
+//                            @Override
+//                            public void onErrorResponse(VolleyError error) {
+//                                Log.e("onErrorResponse: ", error.getMessage());
+//                                //Toast.makeText(MainActivity.this, "" + error.getMessage(), Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
+//
+//                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+//                requestQueue.add(stringRequest);
             }
         });
 
         Profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this,ProfileActivity.class);
-                i.putExtra("username",username.getText().toString());
+                Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                i.putExtra("username", username.getText().toString());
                 startActivity(i);
             }
         });
+
+    }
+
+    private void getJSON(String url) {
+
+
 
     }
 }
---
+++ app/src/main/java/com/emon/emonsassessment/ApiAdapter.java
@@ -0,0 +1,4 @@
+package com.emon.emonsassessment;
+
+public class ApiAdapter {
+}
